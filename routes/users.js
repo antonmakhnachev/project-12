0e0e5956084a48d0f4319580eb0ea7abdb7e403f
Anton Makhnachev <makhnachev.anton@yandex.ru>
2020-04-11 14:59:26 +0300

    added promises to cards.js and users.js

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,34 @@
 const routerUsers = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const usersData = require('../data/users.json');
 
 routerUsers.get('/', (req, res) => {
-  res.send(usersData);
+  fsPromises.readFile(path.join(__dirname, '../data/users.json'))
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+
+    .catch((err) => {
+      res.status(404).send({ massege: err });
+    });
 });
 
 routerUsers.get('/:id', (req, res) => {
-  const user = usersData.find(user => req.params.id === user._id);
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(400).send({ message: 'Нет пользователя с таким id' });
-  }
-  res.send(user);
+  fsPromises.readFile(path.join(__dirname, '../data/users.json'))
+    .then((data) => {
+      const user = JSON.parse(data).find(user => req.params.id === user._id);
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+      };
+    })
+
+    .catch((err) => {
+      res.status(404).send({ massege: err });
+    });
 });
 
 module.exports = routerUsers;
