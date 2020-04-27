77fb565fa9303fae37c380b38e2d25646489e1dc
Anton Makhnachev <makhnachev.anton@yandex.ru>
2020-04-27 19:31:10 +0300

    fixed check by cardId

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,8 +19,14 @@ module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
 
   Card.findByIdAndRemove(cardId)
-    .orFail(() => { throw new Error(); })
-    .then((card) => res.send({ data: card }))
+    .orFail()
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        res.status(404).send({ massege: 'Карточка не найдена' });
+      }
+    })
     .catch((err) => {
       let status = 500;
       let message = 'Произошла ошибка';
